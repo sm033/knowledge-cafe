5cd0608633207ca6d483d9748764a4cf80079af4
mithila akter <mithila.sm03@gmail.com>
2023-09-13 20:55:16 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+import Blogs from './Components/Header/Blogs/Blogs'
+import Header from './components/Header/header'
+
+function App() {
+
+  return (
+    <>
+      <Header></Header>
+      <Blogs></Blogs>
+
+    </>
+  )
+}
+
+export default App
---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,11 @@
+
+
+const Header = () => {
+    return (
+        <div>
+            <h1 className="text-[2.5rem] font-bold">Knowledge Cafe</h1>
+        </div>
+    );
+};
+
+export default Header;
